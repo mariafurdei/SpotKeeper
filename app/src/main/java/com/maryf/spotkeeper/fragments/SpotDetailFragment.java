34b1939b834fc8c087c09cd06eb4a11857d2cbb7
mariafurdei <furdey.mariia@gmail.com>
2018-01-10 16:46:52 -0800

    Current location button becomes enabled depending on On/Off GPS on the phone.

--- app/src/main/java/com/maryf/spotkeeper/fragments/SpotDetailFragment.java
+++ app/src/main/java/com/maryf/spotkeeper/fragments/SpotDetailFragment.java
@@ -1,11 +1,14 @@
 package com.maryf.spotkeeper.fragments;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Address;
 import android.location.Geocoder;
 import android.location.Location;
+import android.location.LocationManager;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
@@ -28,6 +31,9 @@ import android.widget.TextView;
 
 
 import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationAvailability;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.places.GeoDataClient;
 import com.google.android.gms.location.places.Places;
 import com.google.android.gms.location.places.PlaceDetectionClient;
@@ -42,7 +48,6 @@ import com.google.android.gms.maps.OnStreetViewPanoramaReadyCallback;
 import com.google.android.gms.maps.StreetViewPanorama;
 import com.google.android.gms.maps.SupportStreetViewPanoramaFragment;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -52,12 +57,14 @@ import com.maryf.spotkeeper.model.Spot;
 import java.io.IOException;
 import java.util.List;
 
+import static android.content.Context.LOCATION_SERVICE;
+
 
 /**
  * Created by maryf on 4/10/2017.
  */
 
-public class SpotDetailFragment extends Fragment implements OnMapReadyCallback {
+public class SpotDetailFragment extends Fragment implements OnMapReadyCallback, android.location.GpsStatus.Listener {
 
     private GoogleMap mMap;
     private MapView mapView;
@@ -67,10 +74,19 @@ public class SpotDetailFragment extends Fragment implements OnMapReadyCallback {
     private static final String TAG = "SpotDetailFragment";
     private Location mLastKnownLocation;
     private static final float DEFAULT_ZOOM = 15;
-    private static final LatLng mDefaultLocation = new LatLng (33.8121, 117.9190);
+    private static final LatLng mDefaultLocation = new LatLng(33.8121, -117.9190);
+    private static final String KEY_CAMERA_POSITION = "camera_position";
+    private static final String KEY_LOCATION = "location";
 
     private static final int PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1;
     private boolean mLocationPermissionGranted;
+    private boolean mGPSEnabled;
+    private LocationCallback mLocationCallback = null;
+
+    @Override
+    public void onGpsStatusChanged(int event) {
+        
+    }
 
     public interface SpotDetailFragmentListener {
         void onCloseDetailsClick();
@@ -99,6 +115,15 @@ public class SpotDetailFragment extends Fragment implements OnMapReadyCallback {
         this.listener = (SpotDetailFragmentListener) context;
     }
 
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (savedInstanceState != null) {
+            Location mCurrentLocation = savedInstanceState.getParcelable(KEY_LOCATION);
+            Location mCameraPosition = savedInstanceState.getParcelable(KEY_CAMERA_POSITION);
+        }
+    }
+
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstance) {
 
@@ -112,7 +137,6 @@ public class SpotDetailFragment extends Fragment implements OnMapReadyCallback {
 
         // Construct a FusedLocationProviderClient.
         mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(getActivity());
-
 
         Button butRetToSpotsList = (Button) rootView.findViewById(R.id.return_to_spots_list);
         butRetToSpotsList.setOnClickListener(new View.OnClickListener() {
@@ -184,16 +208,51 @@ public class SpotDetailFragment extends Fragment implements OnMapReadyCallback {
             public void afterTextChanged(Editable s) {
                 if (!addressView.getText().toString().matches("")) {
                     LatLng address = addressToPosition(addressView.getText().toString());
-                    addNewMarker(address, addressView.getText().toString());
-                    setPanoramaStreetView(address.latitude, address.longitude);
+                    if (address != null) {
+                        addNewMarker(address, addressView.getText().toString());
+                        setPanoramaStreetView(address.latitude, address.longitude);
+                    }
                 }
             }
         });
 
+        getLocationPermission();
+
+        LocationManager service = (LocationManager) getActivity().getSystemService(LOCATION_SERVICE);
+
+        if (ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return rootView;
+        }
+        mGPSEnabled = service.isProviderEnabled(LocationManager.GPS_PROVIDER);
+
+        service.addGpsStatusListener(this);
+
         setHasOptionsMenu(true);
 
-      return rootView;
+        return rootView;
     }
+
+    @Override
+    public void onDestroyView() {
+        LocationManager service = (LocationManager) getActivity().getSystemService(LOCATION_SERVICE);
+        service.removeGpsStatusListener(this);
+        if (mLocationCallback != null) {
+            mFusedLocationProviderClient.removeLocationUpdates(mLocationCallback);
+            mLocationCallback = null;
+        }
+        super.onDestroyView();
+    }
+
+//    @Override
+//    public void onGpsStatusChanged(int event) {
+//    }
 
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
@@ -230,6 +289,23 @@ public class SpotDetailFragment extends Fragment implements OnMapReadyCallback {
         return super.onOptionsItemSelected(item);
     }
 
+    private LocationRequest getLocationRequest() {
+        LocationRequest locationRequest = new LocationRequest();
+        locationRequest.setInterval(60000);
+        return locationRequest;
+    }
+
+    private void createLocationListener() {
+        mLocationCallback = new LocationCallback() {
+            @Override
+            public void onLocationAvailability(LocationAvailability locationAvailability) {
+                super.onLocationAvailability(locationAvailability);
+                mGPSEnabled = locationAvailability.isLocationAvailable();
+                updateLocationUI();
+            }
+        };
+    }
+
     private void getLocationPermission() {
     /*
      * Request location permission, so that we can get the location of the
@@ -240,6 +316,10 @@ public class SpotDetailFragment extends Fragment implements OnMapReadyCallback {
                 android.Manifest.permission.ACCESS_FINE_LOCATION)
                 == PackageManager.PERMISSION_GRANTED) {
             mLocationPermissionGranted = true;
+            if (mLocationCallback == null) {
+                createLocationListener();
+                mFusedLocationProviderClient.requestLocationUpdates(getLocationRequest(), mLocationCallback, null);
+            }
         } else {
             ActivityCompat.requestPermissions(getActivity(),
                     new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
@@ -247,6 +327,7 @@ public class SpotDetailFragment extends Fragment implements OnMapReadyCallback {
         }
     }
 
+    @SuppressLint("MissingPermission")
     @Override
     public void onRequestPermissionsResult(int requestCode,
                                            @NonNull String permissions[],
@@ -258,6 +339,10 @@ public class SpotDetailFragment extends Fragment implements OnMapReadyCallback {
                 if (grantResults.length > 0
                         && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                     mLocationPermissionGranted = true;
+                    if (mLocationCallback == null) {
+                        createLocationListener();
+                        mFusedLocationProviderClient.requestLocationUpdates(getLocationRequest(), mLocationCallback, null);
+                    }
                 }
             }
         }
@@ -267,7 +352,7 @@ public class SpotDetailFragment extends Fragment implements OnMapReadyCallback {
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-        // Turn on the My Location layer and the related control on the map.
+
         updateLocationUI();
 
         Bundle bundle = getArguments();
@@ -276,19 +361,12 @@ public class SpotDetailFragment extends Fragment implements OnMapReadyCallback {
 
         if (spot != null) {
             spotAddress = spot.getAddress();
-
             updateLocationUI();
-
-            //addressToPosition(spotAddress);
             LatLng address = addressToPosition(spotAddress);
             addNewMarker(address, spotAddress);
             setPanoramaStreetView(address.latitude, address.longitude);
         } else {
-
-            // Turn on the My Location layer and the related control on the map.
             updateLocationUI();
-
-            // Get the current location of the device and set the position of the map.
             getDeviceLocation();
         }
 
@@ -378,14 +456,14 @@ public class SpotDetailFragment extends Fragment implements OnMapReadyCallback {
             return;
         }
         try {
-            if (mLocationPermissionGranted) {
+            if (mLocationPermissionGranted && mGPSEnabled) {
                 mMap.setMyLocationEnabled(true);
                 mMap.getUiSettings().setMyLocationButtonEnabled(true);
             } else {
                 mMap.setMyLocationEnabled(false);
                 mMap.getUiSettings().setMyLocationButtonEnabled(false);
                 mLastKnownLocation = null;
-                getLocationPermission();
+                //getLocationPermission();
             }
         } catch (SecurityException e)  {
             Log.e("Exception: %s", e.getMessage());
@@ -405,24 +483,34 @@ public class SpotDetailFragment extends Fragment implements OnMapReadyCallback {
                     public void onComplete(@NonNull Task<Location> task) {
                         if (task.isSuccessful()) {
                             mLastKnownLocation = task.getResult();
+                            if (mLastKnownLocation != null){
                             LatLng spotOnMap = new LatLng(mLastKnownLocation.getLatitude(), mLastKnownLocation.getLongitude());
                             try {
-                                //addNewMarker(spotOnMap, positionToAddress(mLastKnownLocation.getLatitude(), mLastKnownLocation.getLongitude()));
                                 String address = positionToAddress(mLastKnownLocation.getLatitude(), mLastKnownLocation.getLongitude());
                                 addNewMarker(spotOnMap, address);
                                 setNewAddress(address, mLastKnownLocation.getLatitude(), mLastKnownLocation.getLongitude());
                             } catch (IOException e) {
                                 e.printStackTrace();
+                            }} else {
+                                try {
+                                    String address = positionToAddress(mDefaultLocation.latitude,mDefaultLocation.longitude);
+                                    addNewMarker(mDefaultLocation, address);
+                                    if (mGPSEnabled) {
+                                        setNewAddress(address, mDefaultLocation.latitude, mDefaultLocation.longitude);
+                                    }
+                                } catch (IOException e) {
+                                    e.printStackTrace();
+                                }
+
+                                mMap.getUiSettings().setMyLocationButtonEnabled(false);
                             }
                         } else {
-                            Log.d(TAG, "Current location is null. Using defaults.");
-                            Log.e(TAG, "Exception: %s", task.getException());
-
                             try {
-                                //addNewMarker(mDefaultLocation, positionToAddress(mDefaultLocation.latitude,mDefaultLocation.longitude) );
                                 String address = positionToAddress(mDefaultLocation.latitude,mDefaultLocation.longitude);
                                 addNewMarker(mDefaultLocation, address);
-                                setNewAddress(address, mLastKnownLocation.getLatitude(), mLastKnownLocation.getLongitude());
+                                if (mGPSEnabled){
+                                    setNewAddress(address, mDefaultLocation.latitude, mDefaultLocation.longitude);
+                                }
                             } catch (IOException e) {
                                 e.printStackTrace();
                             }
@@ -445,4 +533,12 @@ public class SpotDetailFragment extends Fragment implements OnMapReadyCallback {
         mMap.animateCamera(CameraUpdateFactory.zoomTo(15), 2000, null);
     }
 
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        if (mMap != null) {
+            outState.putParcelable(KEY_CAMERA_POSITION, mMap.getCameraPosition());
+            outState.putParcelable(KEY_LOCATION, mLastKnownLocation);
+            super.onSaveInstanceState(outState);
+        }
+    }
 }
